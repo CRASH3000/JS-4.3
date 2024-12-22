011cbc7637d8294f61f3347b02753dfcdeb8e440
CRASH3000 <crash-vd@yandex.ru>
2024-12-22 17:51:08 +0300

    first commit

---
+++ src/js/characters/Crossbowman.js
@@ -0,0 +1,4 @@
+import Archer from "./Archer.js";
+import Bow from "../weapons/Bow.js";
+
+export default class Crossbowman extends Archer {}
---
+++ src/js/characters/Demourge.js
@@ -0,0 +1,4 @@
+import Mage from "./Mage.js";
+import StormStaff from "../weapons/StormStaff.js";
+
+export default class Demourge extends Mage {}
